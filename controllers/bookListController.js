6ffc8cf6fb8c61afc0490839003f36f8665aece9
JP Eiler <jp.eiler97@gmail.com>
2021-06-30 18:36:39 -0500

    display saved books in reverse order

--- controllers/bookListController.js
+++ controllers/bookListController.js
@@ -3,25 +3,13 @@ const db = require('../models');
 module.exports = {
     findAll: function(req, res) {
     db.Book
-      .find(req.query)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
-    db.Book
-      .findById(req.params.id)
+      .find(req.query).sort({_id:-1})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     db.Book
       .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Book
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
