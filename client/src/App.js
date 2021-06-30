5a74bc915812ee9a88956918f5fd34dfc1bb70ba
JP Eiler <jp.eiler97@gmail.com>
2021-06-30 14:02:45 -0500

    saved books now displaying

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import SearchPage from './components/SearchPage';
+import SavedPage from './components/SavedPage';
+import SearchPage from './components/SearchPage'
 import Header from './components/Header';
 import Nav from './components/Nav';
 import './styles/styles.css';
@@ -8,7 +9,7 @@ function App() {
 		<div>
 			<Header />
 			<Nav />
-			<SearchPage/>
+			<SavedPage/>
 		</div>
 	);
 }
--- client/src/components/SavedPage.js
+++ client/src/components/SavedPage.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Grid, Container} from '@material-ui/core';
 import Saved from './Saved'
+import API from "../utils/API";
+class SavedPage extends React.Component {
 
-function SavedPage() {
+	state = {
+        savedBooks: []
+      };
 
-	return (
-		<Container maxWidth="md">
-			
-				<Grid container direction="column" justify="center">
-					<Saved></Saved>
-				</Grid>
-			
-		</Container>
-	);
+	  componentDidMount() {
+		this.getBookList();
+	  }
+
+      getBookList() {
+        API.getBooks()
+          .then(res => { console.log(res.data); this.setState({ savedBooks: res.data })})
+          .catch(err => console.log(err));
+      };
+
+	render(){
+		return (
+			<Container maxWidth="md">
+				
+					<Grid container direction="column" justify="center">
+					<Grid container direction="column" justify="center">
+                    {this.state.savedBooks === undefined ? <h1>No Books saved!</h1> : this.state.savedBooks.map((book, i) => {
+                        return <Saved key={book._id} title={book.title} authors={book.authors} description={book.description}
+                        image={book.image} link= {book.link}> </Saved>
+                    })}
+                        
+                    </Grid>
+					</Grid>
+				
+			</Container>
+		);
+	}
+	
 }
 
 export default SavedPage;
